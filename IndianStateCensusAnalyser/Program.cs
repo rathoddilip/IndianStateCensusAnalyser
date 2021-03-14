6cb7e07a7f960d2ce93b8f24edafd052b949b378
diliprathod32 <diliprathod32@gmail.com>
2021-03-14 12:04:30 +0530

    [Dilip] WelcomeToIndianStateCensusAnalyser

---
+++ IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to indian census analyser");
+        }
+    }
+}
